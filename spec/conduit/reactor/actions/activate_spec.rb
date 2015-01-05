2708478dce910ff128f9d58c47a522f059f80c6f
Doug <doug.perez@hellolabs.com>
2015-01-05 13:35:47 -0500

    add activate port

--- spec/conduit/reactor/actions/activate_spec.rb
+++ spec/conduit/reactor/actions/activate_spec.rb
@@ -1,6 +1,59 @@
 require 'spec_helper'
 
 describe Activate do
+  let(:port_attributes) do
+    credentials.merge(nid: '111222333444555667', zip: '33413', operator_name: 'Sprint',
+                        port_info: { mdn: '5555555555', first_name: 'testy', last_name: 'tester', carrier_account: '123456',
+                                    carrier_password: 'password', ssn: '123456789', address1: '123 Test ST' }, mock_status: :success)
+  end
+
+  let(:activate_port) do
+    described_class.new(port_attributes)
+  end
+
+  let(:activate_port_request) do
+    File.read('./spec/fixtures/requests/activate/activate_port.json')
+  end
+
+  describe 'activate_port_request_json' do
+    subject { activate_port.view }
+    it      { should eq activate_port_request }
+  end
+
+  let(:activate_line_id_attributes) do
+    credentials.merge(line_id: 1, mock_status: :success)
+  end
+
+  let(:activate_line_id) do
+    described_class.new(activate_line_id_attributes)
+  end
+
+  let(:activate_line_id_request) do
+    File.read('./spec/fixtures/requests/activate/activate_line_id.json')
+  end
+
+  describe 'activate_line_id_request_json' do
+    subject { activate_line_id.view }
+    it      { should eq activate_line_id_request }
+  end
+
+  let(:activate_attributes) do
+    credentials.merge(nid: '111222333444555667', zip: '33413', operator_name: 'Sprint', mock_status: :success)
+  end
+
+  let(:activate) do
+    described_class.new(activate_attributes)
+  end
+
+  let(:activate_request) do
+    File.read('./spec/fixtures/requests/activate/activate.json')
+  end
+
+  describe 'activate_request_json' do
+    subject { activate.view }
+    it      { should eq activate_request }
+  end
+
   subject do
     described_class.new \
       credentials.merge(line_id: 1, host_override: 'www.hello-labs.com')
@@ -21,6 +74,12 @@ describe Activate do
     subject do
       described_class.new(
         credentials.merge(nid: '111222333444555667', zip: '33413', mock_status: :success)).perform.parser
+    end
+  end
+
+  it_should_behave_like 'parser in progress success response' do
+    subject do
+      described_class.new(port_attributes).perform.parser
     end
   end
 
