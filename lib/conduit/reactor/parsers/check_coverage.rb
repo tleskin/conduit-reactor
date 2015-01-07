6a364700d1f00c999024511b5f9f03d9713ee124
Doug <doug.perez@hellolabs.com>
2015-01-07 09:55:00 -0500

    no need to give block to find attr

--- lib/conduit/reactor/parsers/check_coverage.rb
+++ lib/conduit/reactor/parsers/check_coverage.rb
@@ -2,73 +2,23 @@ require 'conduit/reactor/parsers/base'
 
 module Conduit::Driver::Reactor
   class CheckCoverage::Parser < Parser::Base
-    attribute :csa do
-      object_path('csa')
-    end
-
-    attribute :npa do
-      object_path('npa')
-    end
-
-    attribute :nxx do
-      object_path('nxx')
-    end
-
-    attribute :city do
-      object_path('city')
-    end
-
-    attribute :state do
-      object_path('state')
-    end
-
-    attribute :is3g do
-      object_path('is3g')
-    end
-
-    attribute :affiliate_name do
-      object_path('affiliate_name')
-    end
-
-    attribute :confidence do
-      object_path('confidence')
-    end
-
-    attribute :coverage_quality_cdma do
-      object_path('coverage_quality_cdma')
-    end
-
-    attribute :coverage_quality_iden do
-      object_path('coverage_quality_iden')
-    end
-
-    attribute :evdo do
-      object_path('evdo')
-    end
-
-    attribute :hybrid do
-      object_path('hybrid')
-    end
-
-    attribute :iden do
-      object_path('iden')
-    end   
-
-    attribute :latitude do
-      object_path('latitude')
-    end
-
-    attribute :longitude do
-      object_path('longitude')
-    end
-
-    attribute :zip do
-      object_path('zip')
-    end
-
-    attribute :zip4 do
-      object_path('zip4')
-    end
+    attribute :csa
+    attribute :npa
+    attribute :nxx
+    attribute :city
+    attribute :state
+    attribute :is3g
+    attribute :affiliate_name
+    attribute :confidence
+    attribute :coverage_quality_cdma
+    attribute :coverage_quality_iden
+    attribute :evdo
+    attribute :hybrid
+    attribute :iden
+    attribute :latitude
+    attribute :longitude
+    attribute :zip
+    attribute :zip4
 
     def response_content?
       !@json.nil?
