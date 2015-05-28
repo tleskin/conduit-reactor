f825458f1c03bebd96a69aa98618a3084bba8837
Beardo <jason.moore@hellolabs.com>
2015-05-28 14:57:07 -0600

    Change query subscription to include carrier data and service data

--- lib/conduit/reactor/parsers/query_subscription.rb
+++ lib/conduit/reactor/parsers/query_subscription.rb
@@ -2,73 +2,16 @@ require 'conduit/reactor/parsers/base'
 
 module Conduit::Driver::Reactor
   class QuerySubscription::Parser < Parser::Base
-
-    attribute :status do
-      object_path('status')
+    attribute :carrier_data do
+      object_path('carrier_data')
     end
 
-    attribute :reseller_partner_id do
-      object_path('reseller_partner_id')
-    end
-
-    attribute :mdn do
-      object_path('mdn')
-    end
-
-    attribute :msid do
-      object_path('msid')
-    end
-
-    attribute :csa do
-      object_path('csa')
-    end
-
-    attribute :nid_dec do
-      object_path('esn_dec')
-    end
-
-    attribute :nid_hex do
-      object_path('esn_hex')
-    end
-
-    attribute :imsi do
-      object_path('imsi')
-    end
-
-    attribute :icc_id do
-      object_path('icc_id')
-    end
-
-    attribute :activated_at do
-      object_path('activated_at')
-    end
-
-    attribute :plan_code do
-      object_path('plan_code')
-    end
-
-    attribute :plan_description do
-      object_path('plan_description')
-    end
-
-    attribute :plan_effective_date do
-      object_path('plan_effective_date')
-    end
-
-    attribute :nai do
-      object_path('nai')
-    end
-
-    attribute :nai_effective_date do
-      object_path('nai_effective_date')
-    end
-
-    attribute :service_records do
-      object_path('service_records')
+    attribute :services do
+      object_path('services')
     end
 
     def response_content?
-      !object_path('mdn').nil?
+      object_path('carrier_data').nil?
     end
   end
 end
