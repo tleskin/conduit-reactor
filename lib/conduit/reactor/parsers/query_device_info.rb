4d125656884f92617cc38251ef5e9c60342fc380
Pedro Martinez <pedro.martinez@hellolabs.com>
2015-07-01 23:06:39 -0400

    Updating query_device_info parser

--- lib/conduit/reactor/parsers/query_device_info.rb
+++ lib/conduit/reactor/parsers/query_device_info.rb
@@ -6,23 +6,32 @@ module Conduit::Driver::Reactor
     attribute :model_number
     attribute :manufacturer_name
     attribute :iccid
-    attribute :imsi
-    attribute :uicc_type    
-    attribute :phone_ownership_code
+    attribute :uicc_imsi
+    attribute :uicc_sku
     attribute :availability_type_code
     attribute :availability_type_message
+    attribute :uicc_type
+    attribute :uicc_type_description
     attribute :uicc_compatibility
+    attribute :uicc_compatibility_description
     attribute :uicc_availability_code
     attribute :uicc_availability_message
     attribute :device_type
+    attribute :device_type_description
     attribute :freq_mode
+    attribute :freq_mode_description
     attribute :equipment_freq_type_code
+    attribute :equipment_freq_type_description
     attribute :uicc_not_available_reason_code
-    attribute :uicc_not_available_reason_message
     attribute :not_available_reason_code
-    attribute :not_available_reason_message
-    attribute :device_serial_number
+    attribute :esn_meid_hex
     attribute :validation_message
+    attribute :poc_swap_indicator
+    attribute :poc_swap_indicator_description
+    attribute :device_fed_met_indicator
+    attribute :device_fed_met_indicator_description
+    attribute :activation_status
+    attribute :activation_status_description
 
     def response_content?
       !@json.nil?
--- lib/conduit/reactor/version.rb
+++ lib/conduit/reactor/version.rb
@@ -1,5 +1,5 @@
 module Conduit
   module Reactor
-    VERSION = "0.0.33"
+    VERSION = "0.0.34"
   end
 end
