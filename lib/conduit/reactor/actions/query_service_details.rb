7a9e0f6b7fef1501ccb66c9e4c37d7cd97ad1ae5
Beardo <jason.moore@hellolabs.com>
2015-07-28 11:34:20 -0600

    Add query_service_details action

---
+++ lib/conduit/reactor/actions/query_service_details.rb
@@ -0,0 +1,22 @@
+require 'conduit/reactor/actions/base'
+
+module Conduit::Driver::Reactor
+  class QueryServiceDetails < Conduit::Driver::Reactor::Base
+    url_route           '/lines'
+    required_attributes :mdn
+    optional_attributes :service_period_uuid
+    http_method         :get
+
+    def remote_url
+      super + query_subscription_url
+    end
+
+    private
+
+    def query_subscription_url
+      "/#{@options[:mdn]}/service_details" +
+          (@options[:service_period_uuid] ?
+           "?service_period_uuid=#{@options[:service_period_uuid]}" : "")
+    end
+  end
+end
