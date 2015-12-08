edc28761ed44f8ad0734c5598e1cd53cae4aae81
Beardo <jason.moore@hellolabs.com>
2015-12-08 12:56:30 -0700

    Define http method on get_service_period

--- lib/conduit/reactor/actions/get_service_period.rb
+++ lib/conduit/reactor/actions/get_service_period.rb
@@ -4,6 +4,7 @@ module Conduit::Driver::Reactor
   class GetServicePeriod < Conduit::Driver::Reactor::Base
     url_route           '/service_periods'
     required_attributes :uuid
+    http_method         :get
 
     def remote_url
       super + "/#{@options[:uuid]}"
